7a9f43feaa6d5b6b2fc4dc8bbc995c32e9e97c96
Sadman Ahmed Shanto <shanto@usc.edu>
2024-01-02 12:30:09 -0800

    testing the workflow file - check for prev-titles

--- printing_press.py
+++ printing_press.py
@@ -23,6 +23,7 @@ from feedparser import parse
 from re import search, sub
 from functools import reduce
 import sys
+import os
 from concurrent.futures import ThreadPoolExecutor
 
 class Journalist:
@@ -98,14 +99,14 @@ class Journalist:
             return True
         else:
             return False
-    
-        
+
+
 class Editor:
     '''retrieves the drafts from Journalist and formats them into
        JSON message payloads'''
-       
+
     DIVIDER = {"type": "divider"}
-       
+
     def __init__(self,feeds='feeds.txt',keywords='keywords.txt',authors='authors.txt',channel='channel.txt',nLow=1):
         journalist = Journalist(feeds,keywords,authors,nLow)
         self.timestamp = ""
@@ -113,6 +114,11 @@ class Editor:
             self.channel = journalist._get_strings(f.read())[0]
         self.username = 'PaperBoy'
         self.icon = ":robot_face:"
+
+        # Before opening previous_titles.txt
+        if not os.path.exists('previous_titles.txt'):
+            open('previous_titles.txt', 'w').close()
+
         with open('previous_titles.txt') as f:
             self.titles = journalist._get_strings(f.read())
         self.new_titles = []
@@ -121,7 +127,7 @@ class Editor:
             with open('previous_titles.txt','a') as f:
                 f.write('\n'.join(str(title) for title in self.new_titles))
                 f.write('\n')
-        
+
     def get_payload(self,draft):
         E = draft['entry']
         self.new_titles.append(E['title'])
